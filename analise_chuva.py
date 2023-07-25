225bc44832fbd1e327a65c25dcc0e067111b5636
Mateus Francisco <mateus.francisco@ufpe.br>
2023-07-24 22:22:30 -0300

    ploting cross correlation using matplot.lib

--- analise_chuva.py
+++ analise_chuva.py
@@ -9,6 +9,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import signal
+import scipy.stats
 from statsmodels.tsa.tsatools import detrend
 
 def climatology(df):
@@ -65,6 +66,7 @@ def plotclimatology(clim, fileName):
     fileName = str(fileName)
     plt.savefig('Climatologia_mensal_chuva_' + fileName + '.png', dpi = 300, format='png', bbox_inches="tight")
 
+#LENDO ARQUIVO
 
 df = pd.read_csv('chuvaSERTAOSAOFRANCISCO.csv', sep=';')
 df['Data'] = pd.to_datetime(df['Data'])
@@ -73,7 +75,7 @@ df['Data'] = pd.to_datetime(df['Data'])
 
 climResult = climatology(df)
 
-plotclimatology(climResult, 'SERTAOFRANCISCO')
+# plotclimatology(climResult, 'SERTAOFRANCISCO')
 
 #ANOMALIA
 
@@ -82,23 +84,54 @@ an = pd.DataFrame(anomaly(df, climResult))
 
 an1 = detrend(an[0]);
 
-an2 = signal.detrend(df['mon_pluviometrico_hist_chuva_mm'])
+an2 = detrend(df['mon_pluviometrico_hist_chuva_mm'])
 
-print(pd.DataFrame(an1)[0].corr(pd.DataFrame(an2)[0], method='spearman'))
+# print(pd.DataFrame(an1).corr(pd.DataFrame(an2), method='spearman'))
+# print(pd.DataFrame(an1).corr(an2[0], method='spearman'))
+
+print(scipy.stats.spearmanr(an1, an2)) 
 
 colormat=np.where(an2>0, 'b','r')
 
 fig, ax = plt.subplots(figsize=(20, 5))
 
-ax.bar(df['Data'], an2, width=20,color=colormat)
+ax.bar(df['Data'], an2, width=20, color=colormat)
 
-plt.title('Anomalia de Chuvas mensais RMR Detrend')
+plt.title('Anomalia de Chuvas mensais de Sertão São Francisco (Detrended)')
 plt.xlabel('Tempo')
 plt.ylabel('Chuva (mm)')
 
 
-
-plt.savefig('Anomalia_mensal_chuvas_RMR_teste.png', dpi = 300, format='png', bbox_inches="tight")
+plt.savefig('Anomalia_mensal_chuvas_SertaoSaoFrancisco_DETREND.png', dpi = 300, format='png', bbox_inches="tight")
 
 
 plt.show()
+
+# X CORR
+
+indices = pd.read_csv('APAC-GMMC-NOPE.csv', sep=';')
+indices['Data'] = pd.to_datetime(df['Data'])
+
+fig, ax1 = plt.subplots(figsize=(20, 5))
+
+plt.title('Xcorr Sertão São Francisco - Dipolo')
+plt.xlabel('Lag')
+plt.ylabel('Coefficient Correlation')
+
+# plt.ylim(-1,1)
+
+corr = ax1.xcorr(an2, indices['Dipolo'], usevlines=True, lw=5, normed=True, maxlags=50)
+
+print(max(corr[1]))
+
+plt.savefig('XCorr SertaoSaoFrancisco - Dipolo.png', dpi = 300, format='png', bbox_inches="tight")
+
+'''an1 = np.array(an1)
+an2 = np.array(an2)
+
+corr = np.correlate(a=an1, v=an2)
+corr /= (len(an2) * np.std(an1) * np.std(an2)) # Normalization
+'''
+
+
+
